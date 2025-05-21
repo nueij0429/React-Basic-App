61ba9a86c3b0e571f2964ac002275c140c1d26e2
nueij0429 <ssjjjieun0429@gmail.com>
2025-05-21 10:35:33 +0900

    props 문법

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class MyComponent extends Component {
+  render() {
+    //destructuring assignment
+    const { name, age } = this.props;
+    return (
+      <div>
+        <h3>Hello! {name} / {age}</h3>
+      </div>
+    )
+  }
+} //class
+
+MyComponent.defaultProps = {
+    name: '리액트JS'
+};
+
+MyComponent.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired
+};
