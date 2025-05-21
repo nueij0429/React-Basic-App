15e1dcc09a322af9e923a8193f1a24a723e2fa80
nueij0429 <ssjjjieun0429@gmail.com>
2025-05-21 11:22:17 +0900

    증가, 감소 이벤트 핸들러+onclick

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
       <MyComponent name="ReactJS"/>
       <MyComponent age={100} />
       <hr/>
-      <MyComponentFunc name="I'm 함수형"/>
+      <MyComponentFunc name="I'm 함수형">
+        <p>함수형의 하위 엘리먼트</p>
+      </MyComponentFunc>
     </>
   )
 }
--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -2,13 +2,31 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class MyComponent extends Component {
+  //상태 변수를 포함하는 state 객체 선언
+  state = {
+    value: 0,
+  }
+  //Event Handler 함수
+  handleDecrement = () => (
+    this.setState({
+        value: this.state.value - 1
+    })
+  );
+
   render() {
     //destructuring assignment
     const { name, age } = this.props;
+    const { value } = this.state;
+
     return (
       <div>
         <h2>클래스형 컴포넌트</h2>
         <h3>Hello! {name} / {age}</h3>
+        <p>상태변수 value = {value}</p>
+        <button onClick={() => (
+            this.setState({ value: value + 1 })
+        )}>증가</button>
+        <button>감소</button>
       </div>
     )
   }
